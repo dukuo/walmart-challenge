a4de76e008e6a9cc1b3a0bc1eae9751b612f87f8
Dilip Ramirez <dilip.ramirez@gmail.com>
2020-06-23 19:37:14 -0400

    Testing: Refactored api/search/products tests in unit and integration folders

---
+++ walmart-search-engine-app/pages/api/search/products/___tests___/unit/index.test.ts
@@ -0,0 +1,81 @@
+import {
+    isPalindrome,
+    isLongerThanThreeChars
+} from '../../../../../../utils'
+
+describe('palindrome algorithm', () => {
+    it('should detect string as valid palindrome', () => {
+        const expectedAssertion = true
+        const query = 'obbo'
+        const checkPalindrome = isPalindrome(query)
+
+        expect.assertions(1)
+
+        expect(checkPalindrome).toBe(expectedAssertion)
+    })
+    it('should fail with non-palindrome string', () => {
+        const expectedAssertion = false
+        const query = 'asdf'
+        const checkForPalindrome = isPalindrome(query)
+        
+        expect.assertions(1)
+
+        expect(checkForPalindrome).toBe(expectedAssertion)
+    })
+
+    it('should detect phrase as palindrome', () => {
+        const expectedAssertion = true 
+        const query = 'bab a bab'
+        const checkForPalindrome = isPalindrome(query)
+
+        expect.assertions(1)
+
+        expect(checkForPalindrome).toBe(expectedAssertion)
+    })
+
+    it('should fail on a non-palindrome phrase', () => {
+        const expectedAssertion = false
+        const query = 'asooi fjjs ldjnñ'
+        const checkForPalindrome = isPalindrome(query)
+
+        expect.assertions(1)
+        expect(checkForPalindrome).toBe(expectedAssertion)
+    })
+
+    it('should detect number as palindrome', () => {
+        const query = '181'
+        const expectedAssertion = true
+        const checkForPalindrome = isPalindrome(query)
+
+        expect.assertions(1)
+        expect(checkForPalindrome).toBe(expectedAssertion)
+    })
+
+    it('should fail on non-palindrome number', () => {
+        const query = '1239'
+        const expectedAssertion = false
+        const checkForPalindrome = isPalindrome(query)
+
+        expect.assertions(1)
+        expect(checkForPalindrome).toBe(expectedAssertion)
+    })
+
+    it('is longer than 3 characters', () => {
+        const expectedAssertion = true
+        const query = 'sdda'
+        const checkLength = isLongerThanThreeChars(query)
+
+        expect.assertions(1)
+        expect(checkLength).toBe(expectedAssertion)
+    })
+
+    it('is not longer than 3 characters', () => {
+        const expectedAssertion = false
+        const query = 'sda'
+        const checkLength = isLongerThanThreeChars(query)
+
+        expect.assertions(1)
+        expect(checkLength).toBe(expectedAssertion)
+    })
+    
+})
