0d5d19628aa0a0d28f0607482a5d942d11446a21
Dilip Ramirez <dilip.ramirez@gmail.com>
2020-06-24 17:38:42 -0400

    i18n: Constants for texts, form names (formik) and routes

---
+++ walmart-search-engine-app/constants/router.ts
@@ -0,0 +1,2 @@
+export const API_URI = '/api'
+export const API_SEARCH_PRODUCT = `${API_URI}/search/product`
---
+++ walmart-search-engine-app/constants/text.ts
@@ -0,0 +1,2 @@
+export const TXT_SEARCH_BAR_PLACEHOLDER = '¿Qué estás buscando?'
+export const TXT_BUTTON_LEGAL = 'Ver Información Legal'
