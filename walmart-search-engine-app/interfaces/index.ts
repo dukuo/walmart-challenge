e9bbec351b1ff149b703e1f15aa0baa52d3e4514
Dilip Ramirez <dilip.ramirez@gmail.com>
2020-06-24 17:36:21 -0400

    Typescript: Added ProductRecord interface

--- walmart-search-engine-app/interfaces/index.ts
+++ walmart-search-engine-app/interfaces/index.ts
@@ -4,7 +4,13 @@ export type User = {
   id: number
   name: string
 }
-
+export interface ProductRecord {
+  id: number
+  brand: string
+  description: string
+  image: string
+  price: number
+}
 export interface Product extends Document {
   id: number
   brand: string
