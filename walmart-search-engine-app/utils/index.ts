68eb8f91ea77980355d1e621be1ac0a86829a27e
Dilip Ramirez <dilip.ramirez@gmail.com>
2020-06-24 17:38:10 -0400

    Utils: Palindrome algorithm & checkLength implemented

--- walmart-search-engine-app/utils/index.ts
+++ walmart-search-engine-app/utils/index.ts
@@ -1,8 +1,17 @@
 
-const checkLength = (query:String, length:Number) => {} // replace with string length check function
+import { ProductRecord } from '../interfaces'
 
-export const isPalindrome = (query:String) => {} // replace with palindrome function
+const checkLength = (query:string, length:number):boolean => query.length === length // replace with string length check function
 
-export const isLongerThanThreeChars = (query:String) => checkLength(query, 4)
+export const applyTransformation = (objects:Array<ProductRecord>, transformation:any):Array<ProductRecord> => objects.map(transformation)
 
-export const applyTransformation = (object:Object) => {}
+export const isPalindrome = (query:string):boolean => {
+    const sanitizedQuery = query.toLowerCase().replace(/[\W_]/g, '')
+    for(let i = 0; i < sanitizedQuery.length; i++) {
+        if(sanitizedQuery[i] !== sanitizedQuery[sanitizedQuery.length - i - 1])
+            return false
+    }
+    return true
+}
+
+export const isLongerThanThreeChars = (query:string) => checkLength(query, 4)
